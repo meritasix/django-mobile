22aa28b49c5abb45c02c9307875c4a00015e0ee7
Merita Six <meritasix@gmail.com>
2026-05-04 00:53:49 -0700

    Update for django removal of `template_dirs` in django > 2.0

--- django_mobile/loader.py
+++ django_mobile/loader.py
@@ -17,12 +17,12 @@ class Loader(BaseLoader):
             raise TemplateDoesNotExist("Origin passed to get_contents has no loader assigned, so we can't find the template.")
         return origin.loader.get_contents(origin)
 
-    def get_template_sources(self, template_name, template_dirs=None):
+    def get_template_sources(self, template_name):
         template_name = self.prepare_template_name(template_name)
         for loader in self.template_source_loaders:
             if hasattr(loader, 'get_template_sources'):
                 try:
-                    for result in loader.get_template_sources(template_name, template_dirs):
+                    for result in loader.get_template_sources(template_name):
                         yield result
                 except UnicodeDecodeError:
                     # The template dir name was a bytestring that wasn't valid UTF-8.
@@ -39,11 +39,11 @@ class Loader(BaseLoader):
             template_name = settings.FLAVOURS_TEMPLATE_PREFIX + template_name
         return template_name
 
-    def load_template(self, template_name, template_dirs=None):
+    def load_template(self, template_name):
         template_name = self.prepare_template_name(template_name)
         for loader in self.template_source_loaders:
             try:
-                return loader(template_name, template_dirs)
+                return loader(template_name)
             except TemplateDoesNotExist:
                 pass
         raise TemplateDoesNotExist("Tried %s" % template_name)
@@ -53,9 +53,7 @@ class Loader(BaseLoader):
         for loader in self.template_source_loaders:
             if hasattr(loader, 'load_template_source'):
                 try:
-                    return loader.load_template_source(
-                        template_name,
-                        template_dirs)
+                    return loader.load_template_source(template_name)
                 except TemplateDoesNotExist:
                     pass
         raise TemplateDoesNotExist("Tried %s" % template_name)
@@ -75,28 +73,22 @@ class Loader(BaseLoader):
 class CachedLoader(DjangoCachedLoader):
     is_usable = True
 
-    def cache_key(self, template_name, template_dirs, *args):
+    def cache_key(self, template_name, *args):
         if len(args) > 0:  # Django >= 1.9
-            key = super(CachedLoader, self).cache_key(template_name, template_dirs, *args)
+            key = super(CachedLoader, self).cache_key(template_name, *args)
         else:
-            if template_dirs:
-                key = '-'.join([
-                    template_name,
-                    hashlib.sha1(force_bytes('|'.join(template_dirs))).hexdigest()
-                ])
-            else:
-                key = template_name
+            key = template_name
 
         return '{0}:{1}'.format(get_flavour(), key)
 
-    def load_template(self, template_name, template_dirs=None):
+    def load_template(self, template_name):
         key = self.cache_key(template_name, template_dirs)
         template_tuple = self.template_cache.get(key)
 
         if template_tuple is TemplateDoesNotExist:
             raise TemplateDoesNotExist('Template not found: %s' % template_name)
         elif template_tuple is None:
-            template, origin = self.find_template(template_name, template_dirs)
+            template, origin = self.find_template(template_name)
             if not hasattr(template, 'render'):
                 try:
                     template = template_from_string(template)
